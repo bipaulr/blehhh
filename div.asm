15e8d15581c145cffc03246dda5f76d323d23b91
paul <pchungath31@gmail.com>
2025-10-06 08:31:53 +0530

    16bit

--- div.asm
+++ div.asm
@@ -1,5 +1,8 @@
 MEMORY ADDRESS	OBJECT CODE	    MNEMONICS
-1000	        B8 0A 00    	MOV AX,000A
-1003	        B3 03	        MOV BL,03
-1005	        F6 F3	        DIV BL
-1007	        F4          	HLT
+2000	        A1 00 11	    MOV AX, [1100]
+2003	        8B 16 02 11	    MOV DX, [1102]
+2007	        8B 1E 04 11	    MOV BX, [1104]
+200B	        F7 F3	        DIV BX
+200D        	A3 06 11	    MOV [1106], AX
+2010        	89 16 08 11	    MOV [1108], DX
+2014        	F4	            HLT
